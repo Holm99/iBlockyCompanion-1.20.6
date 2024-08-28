fca7f11d66344739a77d9233714da2e3f0da646e
Holmeren <ludvig.andreas@hotmail.com>
2024-08-28 15:39:04 +0200

    Initial commit

---
+++ src/main/java/net/holm/boosternoti/IBlockyBoosterNotificationDataGenerator.java
@@ -0,0 +1,11 @@
+package net.holm.boosternoti;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IBlockyBoosterNotificationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/holm/boosternoti/iBlockyBoosterNotificationClient.java
@@ -0,0 +1,10 @@
+package net.holm.boosternoti;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class iBlockyBoosterNotificationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
